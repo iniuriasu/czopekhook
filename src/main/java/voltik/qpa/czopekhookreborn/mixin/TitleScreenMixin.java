5dbf983159bfd1480e8b36a293be17bf53a3ce65
fed <161624491+iniuriasu@users.noreply.github.com>
2025-11-22 19:28:42 +0100

    custom splash text

--- src/main/java/voltik/qpa/czopekhookreborn/mixin/TitleScreenMixin.java
+++ src/main/java/voltik/qpa/czopekhookreborn/mixin/TitleScreenMixin.java
@@ -2,11 +2,10 @@ package voltik.qpa.czopekhookreborn.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,6 +13,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
@@ -35,4 +35,15 @@ public class TitleScreenMixin {
         ci.cancel();
     }
 
-}
+    @Shadow
+    private SplashTextRenderer splashText;
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void changeSplashText(CallbackInfo ci) {
+        this.splashText = new SplashTextRenderer("voltik qpa");
+    }
+    }
+
+
+
+
