cc6f59c0b1bd350932496606e80c03fde4bfa661
groszus <79080481+groszus@users.noreply.github.com>
2025-11-22 20:03:59 +0100

    FIX

--- src/main/java/voltik/qpa/czopekhookreborn/mixin/TitleScreenMixin.java
+++ src/main/java/voltik/qpa/czopekhookreborn/mixin/TitleScreenMixin.java
@@ -1,9 +1,7 @@
 package voltik.qpa.czopekhookreborn.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.LogoDrawer;
 import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.render.RenderLayer;
@@ -15,21 +13,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
-
-
     @Unique
     private static final Identifier PATH = Identifier.of("czopekhookreborn", "textures/gui/czopekhookreborn.png");
     @Unique
     private static final Identifier LOGO_PATH = Identifier.of("czopekhookreborn", "textures/gui/minecrafttitle.png");
+
     @Inject(method = "renderPanoramaBackground", at = @At("HEAD"), cancellable = true)
     private void injectCustomBackground(DrawContext context, float delta, CallbackInfo ci) {
-
         int w = context.getScaledWindowWidth();
         int h = context.getScaledWindowHeight();
         RenderSystem.enableBlend();
@@ -50,24 +43,18 @@ public class TitleScreenMixin {
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LogoDrawer;draw(Lnet/minecraft/client/gui/DrawContext;IF)V"), cancellable = true)
     private void replaceLogo(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        int logoWidth = 512;
-        int logoHeight = 128;
-        int x = (context.getScaledWindowWidth() - logoWidth) / 2;
-        int y = 30;
+        float scale = context.getScaledWindowWidth() * 0.5f / 512f;
+        float x = (context.getScaledWindowWidth() - 512f * scale) / 2f;
 
-        context.drawTexture(
-                RenderLayer::getGuiTextured,
-                LOGO_PATH,
-                x, y,
-                0, 0,
-                logoWidth, logoHeight,
-                logoWidth, logoHeight
-        );
-//groszus fixnie ok
+        context.getMatrices().push();
+        context.getMatrices().translate(x, 30f, 0f);
+        context.getMatrices().scale(scale, scale, scale);
+
+        context.drawTexture(RenderLayer::getGuiTextured, LOGO_PATH, 0, 0, 0, 0, 512, 100, 512, 100);
+
+        context.getMatrices().pop();
         ci.cancel();
     }
-
-
 }
 
 
