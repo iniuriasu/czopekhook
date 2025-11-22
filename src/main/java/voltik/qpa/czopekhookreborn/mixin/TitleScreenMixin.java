29c1bef36e3c9efb011b37904cab2811631be44e
fed <161624491+iniuriasu@users.noreply.github.com>
2025-11-22 19:16:51 +0100

    mainscreen work

---
+++ src/main/java/voltik/qpa/czopekhookreborn/mixin/TitleScreenMixin.java
@@ -0,0 +1,38 @@
+package voltik.qpa.czopekhookreborn.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+
+
+    @Unique
+    private static final Identifier PATH =
+            Identifier.of("czopekhookreborn", "textures/gui/czopekhookreborn.png");
+    @Inject(method = "renderPanoramaBackground", at = @At("HEAD"), cancellable = true)
+    private void injectCustomBackground(DrawContext context, float delta, CallbackInfo ci) {
+
+        int w = context.getScaledWindowWidth();
+        int h = context.getScaledWindowHeight();
+        RenderSystem.enableBlend();
+        context.drawTexture(RenderLayer::getGuiTextured, PATH, 0, 0, 0, 0, w, h, w, h);
+
+        RenderSystem.disableBlend();
+
+        ci.cancel();
+    }
+
+}
