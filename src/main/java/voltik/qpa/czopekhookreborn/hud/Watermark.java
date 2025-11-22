b3d87c6d683ce9e502d4ff0d8432fcc5c5b6d368
duzacipka854 <duzacipka85455@int.pl>
2025-11-22 13:10:53 +0100

    zaawansowana metoda kodu
    r

--- src/main/java/voltik/qpa/czopekhookreborn/hud/Watermark.java
+++ src/main/java/voltik/qpa/czopekhookreborn/hud/Watermark.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class Watermark {
     public static void render(DrawContext context) {
-        context.drawText(MinecraftClient.getInstance().textRenderer, "CzopekHook", 10, 10, Color.WHITE.getRGB(), false);
+        context.drawText(MinecraftClient.getInstance().textRenderer, "CzopekHook RECODE", 10, 10, Color.WHITE.getRGB(), false);
         assert MinecraftClient.getInstance().player != null;
         context.drawText(MinecraftClient.getInstance().textRenderer, "Username " + MinecraftClient.getInstance().player.getName(), 10, 20, Color.WHITE.getRGB(), false);
         context.drawText(MinecraftClient.getInstance().textRenderer, "Server " + MinecraftClient.getInstance().getNetworkHandler().getConnection().getAddress(), 10, 30, Color.WHITE.getRGB(), false);
